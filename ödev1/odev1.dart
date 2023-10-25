d2a5c6ea4755278a1719df26421034c40f8c27f7
Semih Uzun <73335708+semihuzunCE@users.noreply.github.com>
2023-10-25 14:24:29 +0300

    Add files via upload

---
+++ ödev1/odev1.dart
@@ -0,0 +1,97 @@
+void main(List<String> args) {
+  String ilce = "Gölbaşı";
+  String kita = "Afrika";
+  String faks = "+90-212-1111111";
+  String postaKodu = "123658";
+  String instaAdresi = "@Odev1";
+  String calistiginBolum = "Bilgi İşlem";
+  int urunMiktari = 2500;
+  String musteriSoyadi = "Uzun";
+  double odemeMiktari = 1250.90;
+  String dogumTarihi = "01.01.1900";
+  double borc = 10000;
+  bool medeniHal = false; //true: evli, false: bekar
+  String videoYorumu = "video sayesinde hayatim değişti :)";
+  String odemeSaati = "19.45";
+  double eftMiktari = 3000;
+  int satilanMiktar = 6500;
+  String telefonModeli = "s22 ultra";
+  String dergiAdi = "Bilim Çocuk";
+  String basimTarihi = "05.09.2020";
+  double zamMiktari = 25; //yüzde
+  int daireSayisi = 15;
+  double enlem = 22.5;
+  double boylam = 50;
+  String yemekAdi = "Kuru Fasulye";
+  double urunFiyati = 60;
+  String sirket = "Microsoft";
+  String videoAdi = "Flutter Değişkenler";
+  int muzikSuresi = 150; //dk
+  double mekanPuani = 9.0;
+  String dosyaAdi = "odev1.dart";
+  String resimFormati = "png";
+  String renk = "Sİyah";
+  String renkKodu = "0xff969aa0";
+  String bilgisayarModeli = "GE72";
+  double ekranBoyutu = 23.8;
+  double kullanimSuresi = 212; //dk
+  double basinc = 12.8; //bar
+  String etkinlikGunu = "Pazar";
+  String odemeGunu = "02.02.2024";
+  DateTime yolculukCikisTarihi = DateTime(2024, 1, 5);
+  String mahalleAdi = "Baharlar";
+  String otobusHatti = "79KM";
+  double kullanilanDakika = 95;
+  String kargoTakipNo = "AO55462";
+  int kuponSuresi = 15; //gün
+  String kuponKodu = "HG8443922";
+  String faturaTarihi = "02.06.2023";
+
+    print("ilce: $ilce");
+  print("kita: $kita");
+  print("faks: $faks");
+  print("postaKodu: $postaKodu");
+  print("instaAdresi: $instaAdresi");
+  print("calistiginBolum: $calistiginBolum");
+  print("urunMiktari: $urunMiktari");
+  print("musteriSoyadi: $musteriSoyadi");
+  print("odemeMiktari: $odemeMiktari");
+  print("dogumTarihi: $dogumTarihi");
+  print("borc: $borc");
+  print("medeniHal: $medeniHal");
+  print("videoYorumu: $videoYorumu");
+  print("odemeSaati: $odemeSaati");
+  print("eftMiktari: $eftMiktari");
+  print("satilanMiktar: $satilanMiktar");
+  print("telefonModeli: $telefonModeli");
+  print("dergiAdi: $dergiAdi");
+  print("basimTarihi: $basimTarihi");
+  print("zamMiktari: $zamMiktari");
+  print("daireSayisi: $daireSayisi");
+  print("enlem: $enlem");
+  print("boylam: $boylam");
+  print("yemekAdi: $yemekAdi");
+  print("urunFiyati: $urunFiyati");
+  print("sirket: $sirket");
+  print("videoAdi: $videoAdi");
+  print("muzikSuresi: $muzikSuresi");
+  print("mekanPuani: $mekanPuani");
+  print("dosyaAdi: $dosyaAdi");
+  print("resimFormati: $resimFormati");
+  print("renk: $renk");
+  print("renkKodu: $renkKodu");
+  print("bilgisayarModeli: $bilgisayarModeli");
+  print("ekranBoyutu: $ekranBoyutu");
+  print("kullanimSuresi: $kullanimSuresi");
+  print("basinc: $basinc");
+  print("etkinlikGunu: $etkinlikGunu");
+  print("odemeGunu: $odemeGunu");
+  print("yolculukCikisTarihi: $yolculukCikisTarihi");
+  print("mahalleAdi: $mahalleAdi");
+  print("otobusHatti: $otobusHatti");
+  print("kullanilanDakika: $kullanilanDakika");
+  print("kargoTakipNo: $kargoTakipNo");
+  print("kuponSuresi: $kuponSuresi");
+  print("kuponKodu: $kuponKodu");
+  print("faturaTarihi: $faturaTarihi");
+}
